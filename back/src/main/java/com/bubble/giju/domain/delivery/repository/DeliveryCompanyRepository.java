6429fb407e01a43ee9925fbb37cf5190cb83fccc
hwan4338 <rlackdghks8@naver.com>
2025-06-21 19:59:32 +0900

    feat: DeliveryCompany SQL 파일 삭제 후 초기 데이터 삽입

--- back/src/main/java/com/bubble/giju/domain/delivery/repository/DeliveryCompanyRepository.java
+++ back/src/main/java/com/bubble/giju/domain/delivery/repository/DeliveryCompanyRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DeliveryCompanyRepository extends JpaRepository<DeliveryCompany, Integer> {
+    boolean existsByName(String companyName);
 }
