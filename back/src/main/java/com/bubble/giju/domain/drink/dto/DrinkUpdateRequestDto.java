250fc6fe1847e5884254d4148810508bcaf50f7d
hwan4338 <rlackdghks8@naver.com>
2025-06-29 16:20:35 +0900

    test: drinkService 테스트 추가

--- back/src/main/java/com/bubble/giju/domain/drink/dto/DrinkUpdateRequestDto.java
+++ back/src/main/java/com/bubble/giju/domain/drink/dto/DrinkUpdateRequestDto.java
@@ -1,9 +1,11 @@
 package com.bubble.giju.domain.drink.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class DrinkUpdateRequestDto {
 
     @Schema(description = "술 가격")
@@ -14,4 +16,6 @@ public class DrinkUpdateRequestDto {
     private String region;
     @Schema(description = "카테고리 Id 변경")
     private int categoryId;
+
+
 }
